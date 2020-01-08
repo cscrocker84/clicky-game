362908af8f40a1d08a2bb06e4d78772dc5b87f07
Charlie Crocker <cscrocker84@gmail.com>
2020-01-07 21:08:24 -0600

    Heroku, my old nemesis

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -5,7 +5,7 @@ import "./Jumbotron.css";
 
 const Jumbotron = () => (
 	<header className = "header">
-		<h1>Web Stack Logo Clicky Game!</h1>
+		<h1>Web Stack NFL Logo Clicky Game!</h1>
 		<h2>Click on any image to earn a point, but don't click on the same logo more than once. Click all 12 pics, and you win.</h2>
 	</header>
 );
